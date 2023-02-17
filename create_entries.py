324d31656c72088bb55cdde3dbb523f77648721a
Stephan Garland <stephan.marc.garland@gmail.com>
2023-02-17 12:30:57 -0500

    feat: added speedup, as well as email and phone (#5)

--- create_entries.py
+++ create_entries.py
@@ -23,6 +23,7 @@ from utilities.constants import (
     JSON_OBJ_MAX_KEYS,
     JSON_OBJ_MAX_VALS,
     MYSQL_INT_MIN_MAX,
+    PHONE_NUMBER,
 )
 from utilities import utilities
 
@@ -90,6 +91,8 @@ class Generator:
             "timestamp",
             "text",
             "json",
+            "email",
+            "phone",
         ]
         pks = []
         errors = {}
@@ -116,6 +119,20 @@ class Generator:
                 ]
             if col_pk:
                 pks.append(k)
+            if k == "phone" and "char" not in col_type:
+                _add_error(
+                    errors,
+                    (k, "type"),
+                    v,
+                    f"column `{k}` must be of type CHAR or VARCHAR",
+                )
+            if k == "phone" and col_unique:
+                _add_error(
+                    errors,
+                    (k, "unique"),
+                    v,
+                    f"unique is not a valid option for column `{k}` - this is a performance decision; numbers are still unlikely to collide",
+                )
             if not col_type:
                 _add_error(
                     errors,
@@ -135,7 +152,7 @@ class Generator:
                     errors,
                     (k, "width"),
                     v,
-                    f"column type `{col_type}` is not supported",
+                    f"width is not a valid option for column `{k}` of type `{col_type}`",
                 )
             if col_autoinc and "int" not in col_type:
                 _add_error(
@@ -260,6 +277,9 @@ class Generator:
             msg += f"DROP TABLE IF EXISTS `{tbl_name}`;\n"
         msg += f"CREATE TABLE `{tbl_name}` (\n"
         for col, col_attributes in schema.items():
+            # this may expand in the future
+            if col in ["phone"]:
+                cols[col]["create_ranged_arr"] = True
             col_opts = []
             for k, v in col_attributes.items():
                 match k:
@@ -322,6 +342,8 @@ class Runner:
         self.tbl_cols = tbl_cols
         self.tbl_create = tbl_create
         self.tbl_name = tbl_name
+        _has_monotonic = False
+        _has_unique = False
 
         # exceeding auto_increment capacity is checked at schema validation, but since
         # the user can specify --validate without passing --num, uniques have to be checked here
@@ -336,18 +358,25 @@ class Runner:
                     f"MYSQL_MAX_{v['type'].upper().split()[0]}_SIGNED"
                 ]
 
+            if v.get("auto_inc"):
+                _has_monotonic = True
             if v.get("unique"):
+                _has_unique = True
                 if self.args.num > col_max_val:
                     raise TooManyRowsError(k, self.args.num, col_max_val) from None
+            # if uniquity isn't required, and the requested number of rows is greater
+            # than the column can handle, just set it to the column's max since we can repeat
             else:
                 if self.args.num > col_max_val:
                     self.rand_max_id = col_max_val
                 else:
                     self.rand_max_id = self.args.num
 
-        self.monotonic_id = self.allocator(self.args.num)
-        self.random_id = self.allocator(self.rand_max_id, shuffle=True)
-        self.unique_id = self.allocator(self.args.num, shuffle=True)
+        if _has_monotonic:
+            self.monotonic_id = self.allocator(0, self.args.num)
+        self.random_id = self.allocator(0, self.rand_max_id, shuffle=True)
+        if _has_unique:
+            self.unique_id = self.allocator(0, self.args.num, shuffle=True)
         try:
             with open("content/dates.txt", "r") as f:
                 self.dates = f.readlines()
@@ -380,9 +409,9 @@ class Runner:
             sample_list.append(iterable[idx])
         return sample_list
 
-    def make_row(self, schema: dict, idx: int) -> dict:
+    def make_row(self, schema: dict, idx: int, has_timestamp: bool) -> dict:
         row = {}
-        if any("timestamp" in s.values() for s in schema.values()):
+        if has_timestamp:
             date = self.sample(self.dates, self.args.num)
         for col, opts in schema.items():
             if "id" in col:
@@ -395,7 +424,6 @@ class Runner:
 
                     # these are appended to the right of the deque, so they won't be immediately repeated
                     self.random_id.release(row[col])
-
             elif col == "first_name":
                 random_first = self.sample(self.first_names, self.num_rows_first_names)
                 first_name = f"{random_first}".replace("'", "''")
@@ -414,13 +442,14 @@ class Runner:
 
             elif schema[col]["type"] == "json":
                 json_dict = {}
-                keys = self.sample(
+                json_keys = self.sample(
                     self.wordlist, self.num_rows_wordlist, JSON_OBJ_MAX_KEYS
                 )
-                vals = self.sample(
-                    self.wordlist, self.num_rows_wordlist, JSON_OBJ_MAX_VALS
+                # grab an extra for use with email if needed
+                json_vals = self.sample(
+                    self.wordlist, self.num_rows_wordlist, JSON_OBJ_MAX_VALS + 1
                 )
-                json_dict[keys.pop()] = vals.pop()
+                json_dict[json_keys.pop()] = json_vals.pop()
                 max_rows_pct = float(
                     schema.get(col, {}).get("max_length", DEFAULT_MAX_FIELD_PCT)
                 )
@@ -432,13 +461,30 @@ class Runner:
                     json_arr_len = ceil((JSON_OBJ_MAX_VALS - 1) * max_rows_pct)
                 # make 20% of the JSON objects nested with a list object of length
                 if not idx % 5:
-                    key = keys.pop()
+                    key = json_keys.pop()
                     json_dict[key] = {}
-                    json_dict[key][keys.pop()] = [
-                        vals.pop() for _ in range(json_arr_len)
+                    json_dict[key][json_keys.pop()] = [
+                        json_vals.pop() for _ in range(json_arr_len)
                     ]
                 row[col] = f"'{json.dumps(json_dict)}'"
 
+            elif col == "email":
+                try:
+                    email_domain = json_vals.pop()
+                except UnboundLocalError:
+                    email_domain = self.sample(self.wordlist, self.num_rows_wordlist)
+                try:
+                    email_local = random_first
+                except UnboundLocalError:
+                    email_local = self.sample(
+                        self.first_names, self.num_rows_first_names
+                    )
+                row[col] = f"'{email_local.lower()}@{email_domain}.com'"
+            elif col == "phone":
+                phone_digits = [str(x) for x in range(10)]
+                random.shuffle(phone_digits)
+                phone_str = "".join(phone_digits)
+                row[col] = f"'{PHONE_NUMBER[args.country](phone_str)}'"
             elif schema[col]["type"] == "text":
                 max_rows_pct = float(
                     schema.get(col, {}).get("max_length", DEFAULT_MAX_FIELD_PCT)
@@ -490,8 +536,8 @@ class Runner:
                 chunk_list = vals[i : i + DEFAULT_INSERT_CHUNK_SIZE]
                 for row in chunk_list:
                     insert_rows.append(f"({row}),\n")
-                # if we reach the end of a chunk list, make the multi-insert a commit by swapping
-                # the last comma to a semi-colon
+                # if we reach the end of a chunk list, make the multi-insert statement a single
+                # query by swapping the last comma to a semi-colon
                 insert_rows[-1] = insert_rows[-1][::-1].replace(",", ";", 1)[::-1]
         else:
             for row in vals:
@@ -508,8 +554,9 @@ class Runner:
     def run(self):
         sql_inserts = []
         random.seed(os.urandom(4))
+        _has_timestamp = any("timestamp" in s.values() for s in self.schema.values())
         for i in range(1, self.args.num + 1):
-            row = self.make_row(self.schema, i)
+            row = self.make_row(self.schema, i, _has_timestamp)
             sql_inserts.append(row)
         vals = [",".join(str(v) for v in d.values()) for d in sql_inserts]
         match args.filetype:
--- utilities/utilities.py
+++ utilities/utilities.py
@@ -9,10 +9,14 @@ from textwrap import dedent
 
 
 class Allocator:
-    def __init__(self, id_max: int, shuffle: bool = False):
+    def __init__(
+        self, id_min: int, id_max: int, ranged_arr: bool = False, shuffle: bool = False
+    ):
         random.seed(urandom(8))
         self.c_rand_seed = random.getrandbits(32)
+        self.id_min = id_min
         self.id_max = id_max
+        self.id_range = self.id_max - self.id_min
         try:
             self.lib = ctypes.CDLL("./library/fast_shuffle.so")
         except OSError as e:
@@ -26,10 +30,13 @@ class Allocator:
             ctypes.c_uint32,
             ctypes.c_uint32,
         ]
-        self.id_list_ptr = self.lib.fill_array(self.id_max)
+        if not ranged_arr:
+            self.id_list_ptr = self.lib.fill_array(self.id_max)
+        else:
+            self.id_list_ptr = self.lib.fill_array_range(self.id_min, self.id_max)
         if shuffle:
-            self.lib.shuf(self.id_list_ptr, self.id_max, self.c_rand_seed)
-        self.id_list = (ctypes.c_uint32 * self.id_max).from_address(
+            self.lib.shuf(self.id_list_ptr, self.id_range, self.c_rand_seed)
+        self.id_list = (ctypes.c_uint32 * self.id_range).from_address(
             ctypes.addressof(self.id_list_ptr.contents)
         )
         self.ids = deque(self.id_list)
@@ -63,6 +70,12 @@ class Args:
         )
         parser.add_argument(
             "-c", "--chunk", action="store_true", help="Chunk SQL INSERT statements"
+        )
+        parser.add_argument(
+            "--country",
+            choices=["au", "de", "fr", "ke", "jp", "mx", "ua", "uk", "us"],
+            default="us",
+            help="The country's phone number structure to use if generating phone numbers",
         )
         parser.add_argument(
             "-d", "--debug", action="store_true", help="Print tracebacks for errors"
