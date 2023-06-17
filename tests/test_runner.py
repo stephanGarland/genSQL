7c2a911bd94dde43f3d59c41c3935e2ec59d9f42
Stephan Garland <stephan.marc.garland@gmail.com>
2023-06-17 19:27:09 -0400

    chore: ran black

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -35,26 +35,37 @@ def runner_object():
             "unique": "true",
             "default": "0",
         },
-        "last_modified": {"type": "timestamp", "nullable": "false", "default": "now()"}}
-    mock_tbl_create = "CREATE TABLE `test` (\n  `user_id` bigint unsigned NOT NULL AUTO_INCREMENT,\n " \
-        "`first_name` varchar (255) NOT NULL,\n  `last_name` varchar (255) NULL,\n " \
-        "`email` varchar (255) NULL,\n  `city` varchar (255) NULL,\n  `country` varchar (255) NULL,\n " \
-        "`created_at` timestamp NOT NULL DEFAULT NOW(),\n  `last_updated_at` timestamp NULL DEFAULT NULL " \
-        "ON UPDATE NOW(),\n  PRIMARY KEY (`user_id`),\n  UNIQUE KEY email (`email`)\n) ENGINE=InnoDB "\
+        "last_modified": {"type": "timestamp", "nullable": "false", "default": "now()"},
+    }
+    mock_tbl_create = (
+        "CREATE TABLE `test` (\n  `user_id` bigint unsigned NOT NULL AUTO_INCREMENT,\n "
+        "`first_name` varchar (255) NOT NULL,\n  `last_name` varchar (255) NULL,\n "
+        "`email` varchar (255) NULL,\n  `city` varchar (255) NULL,\n  `country` varchar (255) NULL,\n "
+        "`created_at` timestamp NOT NULL DEFAULT NOW(),\n  `last_updated_at` timestamp NULL DEFAULT NULL "
+        "ON UPDATE NOW(),\n  PRIMARY KEY (`user_id`),\n  UNIQUE KEY email (`email`)\n) ENGINE=InnoDB "
         "AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;\n"
+    )
     mock_unique_cols = []
-    r = runner.Runner(mock_args, mock_schema, mock_tbl_name, mock_tbl_cols, mock_tbl_create, mock_unique_cols)
+    r = runner.Runner(
+        mock_args,
+        mock_schema,
+        mock_tbl_name,
+        mock_tbl_cols,
+        mock_tbl_create,
+        mock_unique_cols,
+    )
     return r
+
 
 @patch("gensql.runner.Runner.sample")
 def test_runner(runner_object):
     r = runner_object
-    mock_full_name = Mock(spec='Garland, Stephan')
+    mock_full_name = Mock(spec="Garland, Stephan")
     mock_external_id = Mock(spec=42)
     mock_last_modified = Mock(spec="1995-05-23 01:23:45")
-    #with patch.object(r, "sample") as mock_sample:
-        #mock_sample.side_effect = ["1995-05-23 01:23:45", 42, "Garland, Stephan"]
-        #result = r.make_row(1, True)
-    side_effect=["1995-05-23 01:23:45", 42, "Garland, Stephan"]
+    # with patch.object(r, "sample") as mock_sample:
+    # mock_sample.side_effect = ["1995-05-23 01:23:45", 42, "Garland, Stephan"]
+    # result = r.make_row(1, True)
+    side_effect = ["1995-05-23 01:23:45", 42, "Garland, Stephan"]
     result = r.make_row(side_effect=side_effect)
     assert result == None
