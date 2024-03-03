92fa48f4133b156ce80a62da841a3f3ce699d488
Stephan Garland <stephan.marc.garland@gmail.com>
2024-03-03 15:20:07 -0500

    feat[wip]: interim refactoring into smaller classes

--- threader.py
+++ threader.py
@@ -3,8 +3,8 @@ from graphlib import TopologicalSorter
 from queue import Queue
 from threading import Thread
 
-from library.generators.geo import Geo
 from library.generators.email import Email
+from library.generators.geo import Geo
 from library.generators.name import FirstName, LastName
 
 CHUNK_SIZE = 10_000
@@ -13,18 +13,9 @@ THREADING_BUFFER_SIZE = 10_000
 
 
 class ThreadedFileWriter:
-    # TODO: break this class up
-    def __init__(self, country: str, file_name: str, num_rows: int):
-        self.buffer_size = THREADING_BUFFER_SIZE
-        self.chunk = []
-        self.country = country
+    def __init__(self, file_name: str, writer_queue: Queue):
         self.file_name = file_name
-        self.first_name = FirstName(NUM_ROWS)
-        self.last_name = LastName(NUM_ROWS)
-        self.email = Email(NUM_ROWS)
-        self.geo = Geo(country, NUM_ROWS)
-        self.num_rows = num_rows
-        self.writer_queue = Queue()
+        self.writer_queue = writer_queue
 
     def csv_header_writer(self, col_names: list):
         with open(self.file_name, "w") as f:
@@ -48,9 +39,14 @@ class ThreadedFileWriter:
                             + "\n"
                         )
 
-    def generate_topo_graph(self, cols: dict[str, dict[str, str]]) -> set:
-        graph = defaultdict(set)
-        for k, v in cols.items():
+
+class ColumnOrderGenerator:
+    def __init__(self, cols: dict[str, dict[str, str]]):
+        self.cols = cols
+
+    def generate_topo_graph(self) -> set:
+        graph: dict = defaultdict(set)
+        for k, v in self.cols.items():
             graph[k]
             for depend in v.get("depends_on", []):
                 if depend is not None:
@@ -59,12 +55,27 @@ class ThreadedFileWriter:
         ts = TopologicalSorter(graph)
         return set(ts.static_order())
 
-    def generate_col_order(self, cols: dict[str, dict[str, str]]) -> dict:
+    def generate_col_order(self) -> dict[str, int]:
         col_order = {}
-        for i, col in enumerate(cols):
+        for i, col in enumerate(self.cols):
             col_order[col] = i
 
         return col_order
+
+
+class RowGenerator:
+    def __init__(self, country: str, file_name: str, num_rows: int):
+        self.col_order_gen = ColumnOrderGenerator
+        self.chunk: list = []
+        self.country = country
+        self.file_name = file_name
+        self.first_name = FirstName(NUM_ROWS)
+        self.last_name = LastName(NUM_ROWS)
+        self.email = Email(NUM_ROWS)
+        self.geo = Geo(country, NUM_ROWS)
+        self.num_rows = num_rows
+        self.writer_queue = Queue()
+        self.writer = ThreadedFileWriter(file_name, self.writer_queue)
 
     def generate_data(self, col_order, data_type, num_rows, return_dict, *args):
         fn_map = {
@@ -78,6 +89,8 @@ class ThreadedFileWriter:
         return_dict[col_order] = fn_map[data_type](*args)
 
     def resolve_arg(self, arg_str, context):
+        # TODO: clean this up, ideally making the contract more definite
+        # so these checks don't need to happen at all
         if isinstance(arg_str, str) and arg_str.startswith("self."):
             resolved_arg = getattr(context, arg_str[5:])
         elif isinstance(arg_str, list) and arg_str[0].startswith("self."):
@@ -87,10 +100,11 @@ class ThreadedFileWriter:
         return resolved_arg
 
     def make_with_threads(self, data_dict: dict):
+        cog_inst = self.col_order_gen(data_dict)
         threads = []
         return_dict = {}
-        topo_graph = self.generate_topo_graph(data_dict)
-        col_order = self.generate_col_order(data_dict)
+        topo_graph = cog_inst.generate_topo_graph()
+        col_order = cog_inst.generate_col_order()
         for col in topo_graph:
             resolved_arg = self.resolve_arg(data_dict[col].get("args", ""), self)
             t = Thread(
@@ -113,18 +127,21 @@ class ThreadedFileWriter:
         data_dict = {
             "first_name": {},
             "last_name": {},
-            "email": {"args": ["self.first_name", "self.last_name"], "depends_on": ["first_name", "last_name"]},
+            "email": {
+                "args": ["self.first_name", "self.last_name"],
+                "depends_on": ["first_name", "last_name"],
+            },
             "phone": {"args": "self.country", "depends_on": ["country"]},
             "country": {"depends_on": ["city"]},
             "city": {},
         }
-        self.csv_header_writer(data_dict.keys())
-        writer = Thread(target=self.writer_thread)
+        self.writer.csv_header_writer(data_dict.keys())
+        writer = Thread(target=self.writer.writer_thread)
         writer.start()
         rows = self.make_with_threads(data_dict)
         for row in rows:
             self.chunk.append(row)
-            if len(self.chunk) >= self.buffer_size:
+            if len(self.chunk) >= THREADING_BUFFER_SIZE:
                 self.writer_queue.put(self.chunk)
                 self.chunk = []
         if self.chunk:
@@ -134,5 +151,7 @@ class ThreadedFileWriter:
 
 
 if __name__ == "__main__":
-    t = ThreadedFileWriter("United States", "test.csv", NUM_ROWS)
-    t.row_builder()
+    row_gen = RowGenerator(
+        country="United States", file_name="test.csv", num_rows=NUM_ROWS
+    )
+    row_gen.row_builder()
